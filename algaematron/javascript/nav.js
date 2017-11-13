437ec7a6e060343377670791e775d7657ab0ddd2
Hannamae Greenfield <hgreenfield@mica.edu>
2017-11-13 10:37:54 -0500

    homepage styles changed

--- algaematron/javascript/nav.js
+++
@@ -1,8 +0,0 @@
-console.log("js runs");
-$(document).on("scroll",function(){
-    if($(document).scrollTop()>100){
-        $("header").removeClass("large").addClass("small");
-    } else{
-        $("header").removeClass("small").addClass("large");
-    }
-});
--- lovenote/index.js
+++
@@ -1,8 +0,0 @@
-// // We may come back to this
-// var browserify = require('browserify')
-// var fs = require('fs');
-//
-// var b = browserify('styles.js');
-// b.transform('brfs');
-//
-// b.bundle().pipe(fs.createWriteStream('bundle.js'));
